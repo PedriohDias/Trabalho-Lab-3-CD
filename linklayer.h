c5dc0b4a9b2d8d16e7d29cdb7d16436aeaac59ea
Pedro56789 <166068265+Pedro56789@users.noreply.github.com>
2024-05-22 17:58:04 +0100

    Add files via upload

--- linklayer.h
+++ linklayer.h
@@ -38,74 +38,13 @@ typedef struct linkLayer{
 #define FALSE 0
 #define TRUE 1
 
-
-
-
-
-
-/* 5 bytes   , Transition conditions */
-#define SET_LENGHT  5 // tamanho do SET
-#define FLAG  0x5c  // ( (0x5c)) 0101 1100 flag de inicio e fim
-#define A_EM  0x03  // (0x03) 0000 0011Campo de Endereço (A) de commandos do Emissor, resposta do Receptor
-#define A_RE  0x01  // (0x01)0000 0001 Campo de Endereço (A) de commandos do Receptor, resposta do Emissor
-
-#define C_SET 0x08 // 0000 1000(0x08) Campo de Controlo - SET (set up)
-#define C_UA  0x06 // 0000 0110(0x06) Campo de Controlo - UA (Unnumbered Acknowledgement)
-
-#define BCC(a,c) (a ^ c) // fazer xor a c
-volatile int STOP=FALSE;
-
-
-
-
-/*  State Machine */
-
-#define Start      0
-#define Flag_Rcv   1
-#define A_Rcv      2
-#define C_Rcv      3
-#define Bcc_Ok     4
-#define Stop_Final 5
-
-
-// datalink_protocol_Slides control
-#define Flag_Frame 0x5c // 01011010
-#define SET_Frame 00000111
-#define DISC_Frame 00001010
-#define UA_Frame 00000110
-/*
-#define RR 000R001
-#define REJ 000r0101
-*/
-
-
-/*
-SET (set up) 0 0 0 0 0 1 1 1
-DISC (disconnect) 0 0 0 0 1 0 1 0
-UA (unnumbered acknowledgment) 0 0 0 0 0 1 1 0
-RR (receiver ready / positive ACK) 0 0 0 R 0 0 0 1
-REJ (reject / negative ACK) 0 0 0 R 0 1 0 1
-
-*/
- struct Parameters{
-off_t filesize; // tamanho fo file ,bytes
-char filename[255]; // nome ficehiro
-int type; // TRANSMITTER | RECEIVER
-int gate; // /dev/ttySx | gate is x
-}parameter;
-
-//static struct Paramenters parameter; 
-
-
-// Opens a conection using the "port" parameters defined in struct linkLayer, returns "-1" on error and "1" on sucess
+// Opens a connection using the "port" parameters defined in struct linkLayer, returns "-1" on error and "1" on sucess
 int llopen(linkLayer connectionParameters);
 // Sends data in buf with size bufSize
 int llwrite(char* buf, int bufSize);
 // Receive data in packet
 int llread(char* packet);
 // Closes previously opened connection; if showStatistics==TRUE, link layer should print statistics in the console on close
-int llclose(int showStatistics);
+int llclose(linkLayer connectionParameters, int showStatistics);
 
 #endif
-
-
